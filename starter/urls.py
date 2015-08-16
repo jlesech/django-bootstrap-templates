9d25726a099bd6e4c4bff6064cf1d55040a09017
Julien Le Sech <julien.le.sech@gmail.com>
2015-08-16 17:55:10 +0200

    Add navbar support.

--- starter/urls.py
+++ starter/urls.py
@@ -3,4 +3,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='starter/index.html'), name='index'),
+    url(r'^about/$', TemplateView.as_view(template_name='starter/about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='starter/contact.html'), name='contact'),
 )
